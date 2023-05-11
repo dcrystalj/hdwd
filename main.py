0eb019169f1930e001ef71351dde2b85166b9420
dcrystalj <dcrystalj@gmail.com>
2023-05-11 21:22:53 +0200

    Add dump option

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import json
+
 from btc_hd_wallet import BIP85DeterministicEntropy
 from hdwallet import BIP32HDWallet, BIP84HDWallet, HDWallet, utils
 
-LANGUAGES = ["english", "french", "italian", "chinese_simplified", "chinese_traditional", "japanese", "korean", "spanish"]
+LANGUAGES = [
+    "english",
+    "french",
+    "italian",
+    "chinese_simplified",
+    "chinese_traditional",
+    "japanese",
+    "korean",
+    "spanish",
+]
 
 
-def get_account_private_key(words: str, language: str, passphrase: str, account: int) -> str:
+def dump_root(words: str, language: str, passphrase: str) -> dict:
+    return HDWallet("BTC").from_mnemonic(words, language, passphrase).dumps()
+
+
+def get_account_private_key(
+    words: str, language: str, passphrase: str, account: int
+) -> str:
     wallet = BIP84HDWallet("BTC").from_mnemonic(words, language, passphrase)
     wallet.clean_derivation()
     wallet.from_index(84, hardened=True)  # BIP84 - native segwit
@@ -13,16 +30,31 @@ def get_account_private_key(words: str, language: str, passphrase: str, account:
     return wallet.xprivate_key() or "Error"
 
 
-def get_account_public_key(words: str, language: str, passphrase: str, account: int) -> str:
+def get_account_public_key(
+    words: str, language: str, passphrase: str, account: int
+) -> str:
     wallet = BIP84HDWallet("BTC").from_mnemonic(words, language, passphrase)
     wallet.clean_derivation()
     wallet.from_index(84, hardened=True)  # BIP84 - native segwit
     wallet.from_index(0, hardened=True)  # bitcoin mainnet
-    wallet.from_index(1, hardened=True)  # account
+    wallet.from_index(account, hardened=True)  # account
     return wallet.xpublic_key() or "Error"
 
 
-def get_receiving_addresses(words: str, language: str, passphrase: str, account: int, n: int) -> list[str]:
+def get_account_44_public_key(
+    words: str, language: str, passphrase: str, account: int
+) -> str:
+    wallet = HDWallet("BTC").from_mnemonic(words, language, passphrase)
+    wallet.clean_derivation()
+    wallet.from_index(44, hardened=True)  # BIP44 - legacy
+    wallet.from_index(0, hardened=True)  # bitcoin mainnet
+    wallet.from_index(account, hardened=True)  # account
+    return wallet.xpublic_key() or "Error"
+
+
+def get_receiving_addresses(
+    words: str, language: str, passphrase: str, account: int, n: int
+) -> list[str]:
     addresses = []
     for i in range(n):
         xpublic_key = get_account_public_key(words, language, passphrase, account)
@@ -41,7 +73,8 @@ def get_new_mnemonic(words: str, language: str, passphrase: str, index: int) -> 
 
 
 def main():
-    print("""
+    print(
+        """
 Select your mnemonic language:
     0) "english"
     1) "french"
@@ -52,10 +85,13 @@ Select your mnemonic language:
     6) "korean"
     7) "spanish"
 \n:: Select [0-7]:
-    """)
+    """
+    )
     language = LANGUAGES[int(input())]
 
-    print("\n:: Enter mnemonic words (eg: 'citizen praise jealous element crash express match'): ")
+    print(
+        "\n:: Enter mnemonic words (eg: 'citizen praise jealous element crash express match'): "
+    )
     words = input()
     assert utils.is_mnemonic(words, language), "Invalid mnemonic words"
 
@@ -63,35 +99,56 @@ Select your mnemonic language:
     passphrase = input()
 
     while True:
-        print("""
+        print(
+            """
 ----------------------------------------
 \n:: Select your command:
     0) Derive new mnemonic (BIP85)
-    1) Account private key - Spendable address for whole account:
-    2) Account public key - Watch-only address for whole account:
-    3) Recieving addresses for account
-    4) Exit
-""")
-        command = int(input())
+    1) Dump root data (private/public keys, entropy, etc...)
+    2) Account private key - Spendable address for whole account:
+    3) Account public key - Watch-only address for whole account:
+    4) Account publick legacy key (BIP44)
+    5) Recieving addresses for account
+    6) Exit
+"""
+        )
+        try:
+            command = int(input())
+        except Exception as e:
+            print("Could not parse input", e)
+            continue
 
-        if command >= 4:
+        if command >= 5:
             break
 
         print("\n:: Enter account index: ")
-        index = int(input())
+
+        index = int(input()) if command != 1 else 0
 
         if command == 0:
             print(get_new_mnemonic(words, language, passphrase, index))
         elif command == 1:
-            print(get_account_private_key(words, language, passphrase, index))
+            print(
+                json.dumps(
+                    dump_root(words, language, passphrase),
+                    sort_keys=True,
+                    indent=2,
+                )
+            )
         elif command == 2:
-            print(get_account_public_key(words, language, passphrase, index))
+            print(get_account_private_key(words, language, passphrase, index))
         elif command == 3:
+            print(get_account_public_key(words, language, passphrase, index))
+        elif command == 4:
+            print(get_account_44_public_key(words, language, passphrase, index))
+        elif command == 5:
             print("\n:: How many addresses would you like to generate: ")
             n = int(input())
-            print(*get_receiving_addresses(words, language, passphrase, index, n), sep="\n")
+            print(
+                *get_receiving_addresses(words, language, passphrase, index, n),
+                sep="\n"
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- test_main.py
+++ test_main.py
@@ -1,37 +1,69 @@
-from .main import get_account_private_key, get_account_public_key, get_receiving_addresses, get_new_mnemonic
+from .main import (
+    get_account_private_key,
+    get_account_public_key,
+    get_receiving_addresses,
+    get_new_mnemonic,
+    dump_root,
+    get_account_44_public_key,
+)
 
 words = "citizen praise jealous element crash express match bottom phone salad cattle total already edge among brand tumble across journey range vibrant script melt solid"
 
 
 def test_get_account_private_key():
     assert (
-        get_account_private_key(words, "english", "my supper duper passphrase", 1) ==
-        "zprvAe1Rde28ww564yRnMYWocN8YuQEsvA2uFzVXMedtdqJ2JfoZ588wN6Agk"
+        get_account_private_key(words, "english", "my supper duper passphrase", 1)
+        == "zprvAe1Rde28ww564yRnMYWocN8YuQEsvA2uFzVXMedtdqJ2JfoZ588wN6Agk"
         "knfh5UGP3W9HTzr6rSXNTxzru6rpBv1oK4NGy1j7AKWXSLv7hx"
     )
 
 
 def test_get_account_public_key():
     assert (
-        get_account_public_key(words, "english", "my supper duper passphrase", 1) ==
-        "zpub6rzn39Z2nJdPHTWFTa3oyW5HTS5NKckkdDR8A33WCAq1BU8hcfTButVAc"
+        get_account_public_key(words, "english", "my supper duper passphrase", 1)
+        == "zpub6rzn39Z2nJdPHTWFTa3oyW5HTS5NKckkdDR8A33WCAq1BU8hcfTButVAc"
         "4y5UCn3JwV6QbWRJa4YUZJkrJe5ctMAN5R1LLufm9qH2ApFpqH"
     )
 
 
+def test_get_account_public_legacy_key():
+    assert (
+        get_account_44_public_key(words, "english", "my supper duper passphrase", 1)
+        == "xpub6CAgQyfkj7JMMjatcJnfafoDA2epAsfU2CRbL9TRzDAULAxSWE8jbn"
+        "TexNz1i3e5A81nMfQPLJmxeSaEBjYGecKzqd4Gw6UdYyoHJsX1FLT"
+    )
+
+
 def test_get_recieving_addresses():
-    assert get_receiving_addresses(words, "english", "my supper duper passphrase", 1, 3) == [
+    assert get_receiving_addresses(
+        words, "english", "my supper duper passphrase", 1, 3
+    ) == [
         "bc1qxkut448jdk3myexxlkhjhxgrlvh9m8g6u5snz8",
         "bc1qe8a2dgut8jss2j030l5t996t6cypnf4y3tvsvc",
         "bc1qd2wwgng5hymf7y5x97cxt5kk8lg2pmtccxh5f5",
     ]
 
+
 def test_get_new_mnemonic():
     assert (
-        get_new_mnemonic(words, "english", "my supper duper passphrase", 0) ==
-        "world energy equip clinic roast place illegal adapt amazing double today moral humble twenty sick benefit exercise give illness february allow place segment habit"
+        get_new_mnemonic(words, "english", "my supper duper passphrase", 0)
+        == "world energy equip clinic roast place illegal adapt amazing double today moral humble twenty sick benefit exercise give illness february allow place segment habit"
     )
     assert (
-        get_new_mnemonic(words, "english", "my supper duper passphrase", 1) ==
-        "isolate jealous omit leaf bring screen oval illness elite verify fruit settle staff theme soap below arrange verify fantasy physical game seven tone negative"
+        get_new_mnemonic(words, "english", "my supper duper passphrase", 1)
+        == "isolate jealous omit leaf bring screen oval illness elite verify fruit settle staff theme soap below arrange verify fantasy physical game seven tone negative"
+    )
+
+
+def test_dump_root():
+    dump = dump_root(words, "english", "my supper duper passphrase")
+
+    assert (
+        dump["entropy"]
+        == "295531de23d326a1a230d2a397c89172e0728c81f8d8ea4049e1d8ef378362ae"
+    )
+    assert (
+        dump["root_xprivate_key"]
+        == "xprv9s21ZrQH143K2gaup2sq8VrwrV5ygNawYB35ZHybn15wuWHjqkx87pBso7"
+        "ER5Esk1EqJ336SaeKWwEKBP5xLhGXsWssxQ6VXMWr5RRc5Xyt"
     )
